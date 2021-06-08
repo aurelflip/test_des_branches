ef5732322386f1f220c564eeee275e00ddfd3449
aurelflip <aur.ama972@gmail.com>
2021-06-09 01:34:03 +0200

    test des branches pour e comerce

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+	20.times do 
+    i = Item.create!(
+      title: Faker::Mountain.name,
+      price: Faker::Number.decimal(l_digits: 2),
+      description: Faker::Marketing.buzzwords,
+      image_url: Faker::Marketing.buzzwords
+    )
+    i.errors.messages
+  end
+	
+puts "%" * 50
+puts "           Base de données remplie !"
+puts "%" * 50
